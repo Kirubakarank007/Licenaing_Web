b064bb9c8731804a74985531ff0ba6c57c35f017
Kirubakarank007 <kirubakaran02002@gmail.com>
2025-05-13 23:05:21 +0530

    update program.cs file

--- Program.cs
+++ Program.cs
@@ -12,20 +12,14 @@ builder.Services.AddScoped<BranchAddressService>();
 
 builder.Services.AddSession();
 builder.Services.AddRazorPages();
-var isDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
 
-var connectionString = isDocker
-    ? builder.Configuration.GetConnectionString("DockerDb")
-    : builder.Configuration.GetConnectionString("localDb");
+var connectionString = builder.Environment.IsDevelopment()
+    ? builder.Configuration.GetConnectionString("localDb")
+    : builder.Configuration.GetConnectionString("DockerDb");
 
+// Register the DbContext with the selected connection string
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
-// var connectionString = builder.Environment.IsDevelopment()
-//     ? builder.Configuration.GetConnectionString("localDb")
-//     : builder.Configuration.GetConnectionString("DefaultConnection");
-
-// builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//     options.UseSqlite(connectionString));
 
 var app = builder.Build();
 
